924888d2eab011f15b85fc6ab255c3751ea86c65
OksigenYasukawa <oksigenmc@gmail.com>
2024-03-22 08:47:14 +0700

    test

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ function Create_Custom_Element(tag, attr_tag, attr_name, value) {
 
 if (site.includes("youtube.com")) {
     Add_Custom_Style(`
+        .ytd-app {
+          margin-right: 160px;
+        }
         #js-custom-element-up {
             font-size: 60px;
             padding: 150px 0;
